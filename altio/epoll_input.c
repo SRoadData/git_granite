eba0b3273a2df9e01283ec819a92c1e6229dbb26
Stas <stas135642@yandex.ru>
2018-10-09 18:46:15 +0300

    Edit epoll_input and cap

--- altio/epoll_input.c
+++ altio/epoll_input.c
@@ -8,26 +8,7 @@
 * the file COPYING.gpl-v3 for details.                                    *
 \*************************************************************************/
 
-/* Listing 63-5 */
 
-/* epoll_input.c
-
-   Example of the use of the Linux epoll API.
-
-   Usage: epoll_input file...
-
-   This program opens all of the files named in its command-line arguments
-   and monitors the resulting file descriptors for input events.
-
-   This program is Linux (2.6 and later) specific.
-*/
-#include <sys/epoll.h>
-#include <fcntl.h>
-#include "tlpi_hdr.h"
-
-#define MAX_BUF     1000        /* Maximum bytes fetched by a single read() */
-#define MAX_EVENTS     5        /* Maximum number of events to be returned from
-                                   a single epoll_wait() call */
 
 int
 main(int argc, char *argv[])
