6d5e6f7166ef7b94fe39aa6864ac8a6c5fc6ed15
Stas <stas135642@yandex.ru>
2018-10-11 18:00:39 +0300

    Edit_new altio/t_select.c

--- altio/t_select.c
+++ altio/t_select.c
@@ -23,12 +23,7 @@
 #include <sys/select.h>sdf
 usageError(const char *progName)
 {
-    fprintf(stderr, "Usage: %s {timeout|-} fd-num[rw]...\n", progName);
-    fprintf(stderr, "    - means infinite timeout; \n");
-    fprintf(stderr, "    r = monitor for read\n");
-    fprintf(stderr, "    w = monitor for write\n\n");
-    fprintf(stderr, "    e.g.: %s - 0rw 1w\n", progName);
-    exit(EXIT_FAILURE);
+    fprintf(stderr, "Usage: %s {
 }
 
 int
