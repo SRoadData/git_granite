955a1cb19db8f22c04b2d58f40446a1ea79705f6
Stas <stas135642@yandex.ru>
2018-10-12 13:08:07 +0300

    self_pipe Edit HEAD

--- altio/self_pipe.c
+++ altio/self_pipe.c
@@ -1,12 +1,4 @@
-/*************************************************************************\
-*                  Copyright (C) Michael Kerrisk, 2018.                   *
-*                                                                         *
-* This program is free software. You may use, modify, and redistribute it *
-* under the terms of the GNU General Public License as published by the   *
-* Free Software Foundation, either version 3 or (at your option) any      *
-* later version. This program is distributed without any warranty.  See   *
-* the file COPYING.gpl-v3 for details.                                    *
-\*************************************************************************/
+
 
 /* Listing 63-9 */
 
