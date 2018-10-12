64b1acf0347c1a7cf9cbd44271efaf277e6207f5
Stas <stas135642@yandex.ru>
2018-10-12 20:10:39 +0300

    self_pipe

--- altio/epoll_input.c
+++ altio/epoll_input.c
@@ -1,5 +1,4 @@
-is941
-is942/*************************************************************************\
+/*************************************************************************\
 *                  Copyright (C) Michael Kerrisk, 2018.                   *
 *                                                                         *
 * This program is free software. You may use, modify, and redistribute it *
